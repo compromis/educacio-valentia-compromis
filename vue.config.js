041d437fdf5cba79922b09a319b7728133d70f26
Guillem <guillem@disedit.com>
2018-12-02 18:59:07 +0100

    Change baseUrl in production

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/evc/'
+    : '/',
   chainWebpack: config => {
     config.module
       .rule('vue')
       .use('vue-svg-inline-loader')
       .loader('vue-svg-inline-loader')
-      .options({ /* ... */ })
   }
 }
